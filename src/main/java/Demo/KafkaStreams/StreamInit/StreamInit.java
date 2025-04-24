9e80b96bb83d8998d8cd08badbfe1b83fec79a23
mahmoudtarek1432 <43779204+mahmoudtarek1432@users.noreply.github.com>
2025-04-24 23:54:22 +0200

    changed bootstrap server

--- src/main/java/Demo/KafkaStreams/StreamInit/StreamInit.java
+++ src/main/java/Demo/KafkaStreams/StreamInit/StreamInit.java
@@ -17,7 +17,7 @@ public class StreamInit{
     StreamInit(Topology topology){
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG,"app-id");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:29092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
 
